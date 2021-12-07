4a606575af857c029f5c7c908271f3f1226e02f4
vladbbk <28141868+vladbbk@users.noreply.github.com>
2021-12-07 08:49:58 +0100

    aoc/2021/07 refactoring, lru_cache usage

--- aoc/2021/07/7-1.py
+++ aoc/2021/07/7-1.py
@@ -15,25 +15,23 @@ with open ( sys.argv[1], "r") as f:
     lines = [ line . strip () for line in f.readlines() ]
 
 # ------------------------------------------------------------------------
-MAX_DEPTH = 2000
-CACHE = [None] * MAX_DEPTH
+from functools import lru_cache
 crab_positions = [ int(c) for c in lines[0] . split (',') ]
 fuel_positions = [0]*max(crab_positions)
 
+@lru_cache(maxsize=None)
 def dist ( n ):
     if n < 2:
         return n
 
-    if CACHE [ n ]:
-        return CACHE [ n ]
+    return n + dist ( n - 1 )
 
-    CACHE [ n ] = n + dist ( n - 1 )
-    return CACHE [ n ]
-
-# fill cache to the full ? 
+# without pre-filling, maximum recursion depth exceeded
+MAX_DEPTH = 2000
 for i in range ( MAX_DEPTH ):
     dist ( i )
 
+# we can keep the same algorith, but use dist func instead
 min = math.inf
 for i in range ( len(crab_positions) ):
     total = sum ( [ dist ( abs( i - crab_pos ) ) for crab_pos in crab_positions ])
