e2628019457199b73372d372214baa175fa98258
vladbbk <28141868+vladbbk@users.noreply.github.com>
2021-12-09 14:32:29 +0100

    aoc/2021/09 - refactoring

--- aoc/2021/09/9-1.py
+++ aoc/2021/09/9-1.py
@@ -14,7 +14,6 @@ with open ( sys.argv[1], "r") as f:
     lines = [ line . strip () for line in f.readlines() ]
 
 # ------------------------------------------------------------------------
-import math
 grid = [ [ int(c) for c in line ] for line in lines ]
 
 min_row, max_row = 0, len ( grid ) - 1
@@ -27,8 +26,8 @@ def adjacent_coordinates (x, y):
     return [ (a, b) for a, b in candidates if a >= min_row and a <= max_row and \
                                               b >= min_col and b <= max_col ]
 
-def is_low_point ( x, y, adjacent_points ):
-    return all ( grid[x][y] < grid[a][b] for (a, b) in adjacent_points)
+def is_low_point ( x, y ):
+    return all ( grid[x][y] < grid[a][b] for (a, b) in adjacent_coordinates ( x, y ) )
 
 def get_baisin_size ( x, y ):
     # set yourself as visited
@@ -37,14 +36,11 @@ def get_baisin_size ( x, y ):
     return 1 + sum ( [ get_baisin_size (a, b) for (a, b) in adjacent_coordinates (x, y) if \
                                        (a, b) not in visited and grid[a][b] != 9 ] )
 
-low_points = [ (x, y) for x in range ( len ( grid ) ) for y in range ( len ( grid[x])) if \
-    is_low_point ( x, y, adjacent_coordinates (x, y)) ]
+low_points = [ (x, y) for x in range ( len (grid) ) for y in range ( len (grid[x])) if is_low_point ( x, y) ]
 
 visited = set ( low_points )
 
 sizes = sorted ( [ get_baisin_size (x, y) for (x, y) in low_points] )
-
-sizes = sorted ( sizes )
 
 prod = 1
 for i in range ( 3 ):
